8fba157d92aec558a28a7ef65b4644220f2df350
Dan-Eric Næss <dan-eric.naess@live.no>
2022-08-03 01:12:25 +0200

    Add Capacitor Android build target

---
+++ android/app/src/main/java/nu/golen/fireanddice/MainActivity.java
@@ -0,0 +1,5 @@
+package nu.golen.fireanddice;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'nu.golen.fireanddice',
+  appName: 'FireAndDice',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
