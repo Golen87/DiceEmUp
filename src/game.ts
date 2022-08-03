edad226d6b1add0e74b8b1f92fc9e4e5494e7434
Dan-Eric Næss <dan-eric.naess@live.no>
2022-08-03 03:44:55 +0200

    Work around status bar colors

--- src/game.ts
+++ src/game.ts
@@ -5,6 +5,17 @@ import { GameoverScene } from "./scripts/scenes/GameoverScene";
 import { OverworldScene } from "./scripts/scenes/OverworldScene";
 import { GameScene } from "./scripts/scenes/GameScene";
 
+import { Capacitor } from '@capacitor/core';
+import { StatusBar, Style } from '@capacitor/status-bar';
+import { SafeAreaController } from '@aashu-dubey/capacitor-statusbar-safe-area';
+
+/* Android specific features */
+if ( Capacitor.getPlatform() != 'web' ) {
+	StatusBar.setOverlaysWebView({ overlay: true });
+	StatusBar.setStyle({ style: Style.Dark });
+	SafeAreaController.injectCSSVariables();
+}
+
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.WEBGL,
 	width: 1920/2,
